0d01df85ad6a272532f0cdfef768d430e79d99c1
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2025-01-02 22:04:28 +0100

    MuseChord fixes in search of next segments

--- src/Orchestrion/internal/MuseChord.cpp
+++ src/Orchestrion/internal/MuseChord.cpp
@@ -51,6 +51,59 @@ dgk::Tick MuseChord::GetEndTick() const
     return GetRestEndTick();
 }
 
+namespace
+{
+// Copied and adapted from mu::engraving::Chord::nextTiedChord() ;
+// that version compaes whether the tuplet() of the next chord is the same as
+// the current chord; I don't know why, but for our purpose this causes a
+// problem.
+const mu::engraving::Chord *GetNextTiedChord(const mu::engraving::Chord &chord)
+{
+  using namespace mu::engraving;
+
+  constexpr auto includeOtherVoicesOnStaff = false;
+  const Segment *nextSeg =
+      chord.segment()->nextCR(chord.track(), includeOtherVoicesOnStaff);
+  if (!nextSeg)
+    return nullptr;
+
+  auto chordRest = dynamic_cast<const ChordRest*>(nextSeg->element(chord.track()));
+  assert(chordRest);
+  if (!chordRest || !chordRest->isChord())
+    return nullptr;
+
+  const Chord *next = toChord(nextSeg->element(chord.track()));
+
+  // If there are more notes in the coming chord, then it can't be a tied chord.
+  if (chord.notes().size() < next->notes().size())
+    return nullptr;
+
+  // saintmatthieu: `sameSize` is also an argument of the original
+  // `Chord::nextTiedChord()`. In our case I think it doesn't matter if the next
+  // chord has less notes ; we can do as though they were implicit.
+  constexpr auto sameSize = false;
+  if (sameSize && chord.notes().size() != next->notes().size())
+    return nullptr; // sizes don't match so some notes can't be tied
+
+  // saintmatthieu: this is the part causing the problem.
+  // if (tuplet() != next->tuplet())
+  // {
+  //   return nullptr; // next chord belongs to a different tuplet
+  // }
+
+  for (const Note *n : chord.notes())
+  {
+    const Tie *tie = n->tieFor();
+    if (!tie)
+      return nullptr; // not tied
+    const Note *nn = tie->endNote();
+    if (!nn || nn->chord() != next)
+      return nullptr; // tied to note in wrong voice, or tied over rest
+  }
+  return next; // all notes in this chord are tied to notes in next chord
+}
+} // namespace
+
 dgk::Tick MuseChord::GetChordEndTick() const
 {
   auto chord =
@@ -59,7 +112,7 @@ dgk::Tick MuseChord::GetChordEndTick() const
   while (chord)
   {
     endTick += chord->actualTicks().ticks();
-    chord = chord->nextTiedChord();
+    chord = GetNextTiedChord(*chord);
   }
   return endTick;
 }
@@ -75,7 +128,8 @@ dgk::Tick MuseChord::GetRestEndTick() const
     if (!rest)
       break;
     endTick += rest->actualTicks().ticks();
-    segment = segment->next1();
+    constexpr auto includeOtherVoicesOnStaff = false;
+    segment = segment->nextCR(m_track.value, includeOtherVoicesOnStaff);
   }
   return endTick;
 }
